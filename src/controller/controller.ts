e4c275e8874ab0f914db2891e7b277cd2e236cba
DUDE <nisbis194@gmail.com>
2025-10-25 21:05:06 +0545

    optimized the get subject api

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -4,12 +4,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getSubject = async (req: Request, res: Response) => {
-  const { semesterName } = req.body;
-  const response = await prisma.subject.findMany({
+  const { subjectName } = req.body;
+  const response = await prisma.subDetail.findMany({
     where: {
-      semesterName,
+      subjectName,
     },
   });
+  const modified = response.reduce<any>((acc, item) => {
+    if (!acc[item.subjectName]) {
+      acc[item.subjectName] = {
+        courseId: item.courseId,
+        subjectName: item.subjectName,
+        semesterName: item.semesterName,
+        note: item.note,
+        year: [{ time: item.year, qn: item.qn }],
+      };
+    } else {
+      acc[item.subjectName].year.push({ time: item.year, qn: item.qn });
+    }
 
-  return res.status(200).json(response);
+    return acc;
+  }, {});
+
+  return res.status(200).json(modified);
 };
