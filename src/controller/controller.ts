54d939dc2b2c7952c4535a63936244da93f878a5
DUDE <nisbis194@gmail.com>
2025-10-29 19:55:04 +0545

    made the api structure and rest architechure

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -5,15 +5,22 @@ const prisma = new PrismaClient();
 
 export const getSubject = async (req: Request, res: Response) => {
   const { semesterName, subjectName, courseId } = req.query;
-  const response = await prisma.subDetail.findMany({
-    where: {
-      semesterName: String(semesterName),
-      courseId: Number(courseId),
-      subjectName: String(subjectName),
-    },
-  });
-
-  return res.status(200).json(response);
+  if (!semesterName || !subjectName || !courseId) {
+    return res.status(400).json({ error: " Enter all Fields to Get" });
+  }
+  try {
+    const response = await prisma.subDetail.findMany({
+      where: {
+        semesterName: String(semesterName),
+        courseId: Number(courseId),
+        subjectName: String(subjectName),
+      },
+    });
+    return res.status(200).json(response);
+  } catch (error: any) {
+    console.error(error);
+    return res.status(500).json({ error: "Server error" });
+  }
 };
 
 // for getting subject name
