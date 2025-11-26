6c6d0c87f64319885fccedee9271d42bde69a564
DUDE <nisbis194@gmail.com>
2025-11-26 22:56:57 +0545

    wrote a auth validator for signup and login using joi library

--- src/controller/login.ts
+++ src/controller/login.ts
@@ -2,4 +2,10 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
-export const signup = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+  res.json({ message: "Login SUccess" });
+};
+
+export const signup = async (req: Request, res: Response) => {
+  res.json({ message: " signin success" });
+};
---
+++ src/middleware/authValidation.ts
@@ -0,0 +1,27 @@
+import joi from "joi";
+import { Response, Request, NextFunction } from "express";
+
+const signupValidation = (req: Request, res: Response, next: NextFunction) => {
+  const schema = joi.object({
+    name: joi.string().min(3).max(50).required(),
+    emial: joi.string().email().required(),
+    passwrod: joi.string().min(4).max(20).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(200).json({ message: "badrequest", error });
+  }
+  next();
+};
+
+const loginValidation = (req: Request, res: Response, next: NextFunction) => {
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(4).max(15).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(200).json({ message: "Bad request", error });
+  }
+  next();
+};
