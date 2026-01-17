c783f43af8d9a3adef295c782b682fb31eb40c57
Dude6561 <nisbis194@gmail.com>
2026-01-17 12:36:14 +0545

    finor fix

--- src/controller/supabase.ts
+++ src/controller/supabase.ts
@@ -1,13 +1,8 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.supabaseUrl
-const supabaseKey = process.env.supabaseKey
-if(!supabaseKey || !supabaseUrl) {
-    throw new Error(
-        
-    "Missing Supabase Env Variable"
-    )
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
+if (!supabaseKey || !supabaseUrl) {
+  throw new Error("Missing Supabase Env Variable");
 }
-export const  supabase = createClient(supabaseUrl, supabaseKey)
-
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
