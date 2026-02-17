7c519c4dc0c64b6bf9d9aca10dc2942b670fbdc6
Nischal Sharma <nisbis194@gmail.com>
2026-02-17 18:33:05 +0545

    completed input api

--- src/controller/dashboard.ts
+++ src/controller/dashboard.ts
@@ -17,36 +17,51 @@ export const Course = async (req: Request, res: Response) => {
   });
 };
 //detail dashboard api
-export const Detail = async(req:Request, res:Response) =>{
-  try{
-  const {subject, year, course, semester} = req.body;
-  const subjectNames = subject.split(",").map((data:any) => data.trim());
-  let years;
-  if( year == "2075-2080"){
-     years = [2075,2076,2078,2079,2080];
+export const Detail = async (req: Request, res: Response) => {
+  try {
+    const { subject, year, course, semester } = req.body;
+    const subjectNames = subject.split(",").map((data: any) => data.trim());
+    let years;
+    if (year == "2075-2080") {
+      years = [2075, 2076, 2078, 2079, 2080];
+    }
+    const data = {
+      subject: subjectNames,
+      year: years,
+      course: course,
+      semester: semester,
+    };
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ Message: " Server Error" });
   }
-  const data ={subject: subjectNames, year: years, course:course, semester:semester};
-  return res.status(200).json(data);
-  } catch (error){
-return res.status(500).json({Message : " Server Error"});
-  }
-}
+};
 
 // for inputiing files in the server storage
-export const Input = async(req:Request, res:Response) =>{
-  try{
-  const { subject, year, course, semester} = req.body;
- if (!req.files || Array.isArray(req.files)) {
-  return res.status(400).json({ message: "Invalid file upload" });
-}
-const image1 = req.files.image1?.[0];
-if(req.files.image2?.[1]){
-const image2 = req.files.image2?.[1];
-const {data : data2 ,error: error2}= await supabase.storage.from(`${course}`).upload(`${semester}/${year}/${image1.originalname}`,image1.buffer);
+export const Input = async (req: Request, res: Response) => {
+  try {
+    const { subject, year, course, semester } = req.body;
+    if (!req.files || Array.isArray(req.files)) {
+      return res.status(400).json({ message: "Invalid file upload" });
+    }
+    const image1 = req.files.image1?.[0];
+    const image2 = req.files.image2?.[0] ?? null;
 
-} // const {data, error } = await supabase.storage.createBucket('')// to make bucket confusion if course comes or id
-const {data : data1 ,error: error1}= await supabase.storage.from(`${course}`).upload(`${semester}/${year}/${image1.originalname}`,image1.buffer);
+    // const {data, error } = await supabase.storage.createBucket('')// to make bucket confusion if course comes or id
+    const { data: data1, error: error1 } = await supabase.storage
+      .from(`${course}`)
+      .upload(`${semester}/${year}/${image1.originalname}`, image1.buffer);
+    if (error1) throw error1;
 
-
-  } catch{}
-}
+    // image 2 is optional
+    if (image2) {
+      const image2 = req.files.image2?.[0];
+      const { data: data2, error: error2 } = await supabase.storage
+        .from(`${course}`)
+        .upload(`${semester}/${year}/${image2.originalname}`, image2.buffer);
+      if (error2) throw error2;
+    }
+  } catch {
+    return res.status(400).json({ message: "Error Uploading File" });
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import express from "express";
 import dashRoute from "./routes/dashRoute";
